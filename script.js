9481051aa48007e8c30c1a1da0fca23aee8ab9cf
Bruno Coelho <brunocoelhonet@gmail.com>
2022-03-23 15:30:28 -0300

    Exercício 3

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
-let numero1 = parseInt(prompt('Digite o primeiro número'))
-let numero2 = parseInt(prompt('Digite o segundo número'))
-let operador = prompt('Digite o operador')
-
-function conta(n1, n2, operador) {
-  switch (operador) {
-    case '+':
-      console.log('Sua soma resultou: ' + (n1 + n2))
-      break
-
-    case '-':
-      console.log('Sua subtração resultou: ' + (n1 - n2))
-      break
-
-    case '*':
-      console.log('Sua multiplicação resultou: ' + n1 * n2)
-      break
-
-    case '/':
-      console.log('Sua divisão resultou: ' + n1 / n2)
-      break
-
-    default:
-      console.log('Operação inválida')
-  }
-}
-
-conta(numero1, numero2, operador)
-
-//chamando a função cinco vezes com todos operadores
-var listaOperadores = ['+', '-', '*', '/', 'operadorInválido']
-
-for (let i = 0; i < listaOperadores.length; i++) {
-  conta(numero1, numero2, listaOperadores[i])
-}
+var numero = parseInt(prompt('Digite um número'))
+numero % 2 == 0
+  ? console.log(numero + ' é um número par')
+  : console.log(numero + ' é um número ímpar')
