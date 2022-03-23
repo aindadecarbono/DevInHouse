fe29b65ef3381e4a28530ec7997b9465f907cb6a
Bruno Coelho <brunocoelhonet@gmail.com>
2022-03-23 13:57:40 -0300

    Exercício 2

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-var idade = parseInt(prompt('Digite a sua idade'))
-while (idade < 0) {
-  idade = parseInt(prompt('Digite a sua idade'))
+let numero1 = parseInt(prompt('Digite o primeiro número'))
+let numero2 = parseInt(prompt('Digite o segundo número'))
+let operador = prompt('Digite o operador')
+
+function conta(n1, n2, operador) {
+  switch (operador) {
+    case '+':
+      console.log('Sua soma resultou: ' + (n1 + n2))
+      break
+
+    case '-':
+      console.log('Sua subtração resultou: ' + (n1 - n2))
+      break
+
+    case '*':
+      console.log('Sua multiplicação resultou: ' + n1 * n2)
+      break
+
+    case '/':
+      console.log('Sua divisão resultou: ' + n1 / n2)
+      break
+
+    default:
+      console.log('Operação inválida')
+  }
 }
 
-if (idade <= 15) {
-  console.log('Pessoa jovem')
-} else if (idade >= 16 && idade <= 64) {
-  console.log('Pessoa adulta')
-} else {
-  console.log('Pessoa idosa')
+conta(numero1, numero2, operador)
+
+//chamando a função cinco vezes com todos operadores
+var listaOperadores = ['+', '-', '*', '/', 'operadorInválido']
+
+for (let i = 0; i < listaOperadores.length; i++) {
+  conta(numero1, numero2, listaOperadores[i])
 }
